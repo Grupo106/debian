b183619e02a682412abb2f46d2b6346954ee59fb
Yonatan Romero <yromero@openmailbox.org>
2016-07-07 14:02:37 -0300

    cambio nombre yml

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ apt-get install -y python-pip python-dev gcc make libffi-dev libssl-dev
 apt-get remove -y python-cryptography
 pip install --upgrade setuptools cryptography markupsafe
 pip install ansible
-echo "alias netcop-update=\"cd ~/netcop &&
+echo "alias netcop-update=\"cd ~/ansible &&
       git pull &&
       sudo ansible-playbook -i 'localhost,' -c local netcop.yml
       cd -;\"" >> ~/.bashrc
