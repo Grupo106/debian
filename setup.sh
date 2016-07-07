ef3f29f2e189d811075712692a87bc8281e17e2c
Yonatan Romero <yromero@openmailbox.org>
2016-07-07 12:23:46 -0300

    Responder si al eliminar paquetes

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 apt-get update
 apt-get upgrade
 apt-get install -y python-pip python-dev gcc make libffi-dev libssl-dev
-apt-get remove python-cryptography
+apt-get remove -y python-cryptography
 pip install --upgrade setuptools cryptography markupsafe
 pip install ansible
