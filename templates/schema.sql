938f3a2f5e6ffe748ff6f78cf195cb3aa184f0d4
Yonatan Romero <yromero@openmailbox.org>
2016-07-12 10:22:10 -0300

    Refactor

--- templates/schema.sql
+++ templates/schema.sql
@@ -6,8 +6,8 @@ CREATE TABLE IF NOT EXISTS paquetes (
     ip_destino integer NOT NULL,
     puerto_destino integer NOT NULL,
     bytes integer NOT NULL,
-    direccion smallint NULL,
-    protocolo smallint NOT NULL
+    direccion smallint NOT NULL DEFAULT 0,
+    protocolo smallint NOT NULL DEFAULT 0
 );
 
 CREATE INDEX hora_idx ON paquetes(hora_captura);
